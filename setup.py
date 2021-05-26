4778e10023b3d744354913066c0423e8e138a04b
Gabriel Acosta <acostadariogabriel@gmail.com>
2021-05-26 20:42:55 -0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='feriado',
+    version='0.1',
+    description='Show next holiday information in Argentina',
+    author='gabox',
+    license='GPLv3+',
+    scripts=['feriado'],
+)
